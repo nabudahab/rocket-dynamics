ef4e021176b14b6eb4802c092307b81121f843c0
nabudahab <20204834+nabudahab@users.noreply.github.com>
2021-12-19 03:06:02 -0500

    check if user-input filename is valid

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import cv2
+from os.path import exists
 
-#Ask user for video file path
-video_fp = input("Video file path: ")
+#Asks user for video filepath and checks if file exists
+def get_video_fp():
+    #Ask user for video file path
+    video_fp = input("Video file path: ")
 
+    #Check file exists, ask user again if it doesn't
+    if not exists(video_fp):
+        print("Filepath invalid.")
+        get_video_fp()
+    else:
+        return video_fp
+
+        
 #Create OpenCV video capture object
-vidcap = cv2.VideoCapture(video_fp)
+vidcap = cv2.VideoCapture(get_video_fp())
 
 #Read first frame from video
 success, image = vidcap.read()
