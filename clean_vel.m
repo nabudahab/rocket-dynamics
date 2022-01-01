fc8e6e39a8291d5335b4b66e1ab3157b46a2acb3
nabudahab <20204834+nabudahab@users.noreply.github.com>
2022-01-01 14:41:16 -0500

    clean clean_vel.m

--- clean_vel.m
+++ clean_vel.m
@@ -3,28 +3,24 @@ function [vel] = clean_vel(filename)
     data = readmatrix(filename);
     data = [data(:,1)' ; data(:,3)']';
 
-    %Time derivative of velocity
-    d_data = diff([eps; data(:,2)])./diff([eps; data(:,1)]);
-
     %Create spike detector array
     spike_detector = zeros(length(data));
 
-    plot(d_data)
-    hold on
-    plot(data(:,1), data(:,2))
-    hold off
-
     %Loop through d_data vectory
-    for i = 2:length(data)
-        %Find single peak/trough spikes
-        if(xor(d_data(i-1) > 0, d_data(i) > 0)) && (abs(abs(d_data(i)) - abs(d_data(i-1))) < 0.2*abs(d_data(i)))
-            spike_detector(i-1) = d_data(i-1);
+    for i = 4:length(data)-3
+        %Find double trough spikes
+        arr = i-3:i+3;
+        arr = arr(arr ~= i);
+        upper_thresh = std(data(arr, 2)) + mean(data(arr, 2));
+        lower_thresh = mean(data(arr, 2)) - std(data(arr, 2));
+        if(data(i, 2) > upper_thresh || data(i, 2) < lower_thresh || (data(i,2) == 0 && data(i-1) > 0))
+            spike_detector(i) = 100;
         end
     end
 
     %This array stores spike locations
     spikes = [];
-
+    
     %Locate spikes through the spike detector array
     for i = 1:length(data)
         if(spike_detector(i) ~= 0)
@@ -32,9 +28,9 @@ function [vel] = clean_vel(filename)
         end
     end
 
-    %Create 'cleaned' version of dataset, removing the spikes.
-    cleaned_data = data;
-    cleaned_data(spikes, :) = [];
-
-    plot(cleaned_data(:,1), cleaned_data(:,2))
+    %Remove spikes
+    vel = data;
+    vel(spikes, :) = [];
+    
+    
 end
